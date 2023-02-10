159e95b05cd5ef0ff7caf2158715d99dc3ed264e
EthanBensler <ethan.bensler@ucalgary.ca>
2023-02-10 14:57:23 -0700

    Question 2 finish up

--- ex2/ex2.2.py
+++ ex2/ex2.2.py
@@ -27,46 +27,6 @@ def func2(array, start, end):
   array[start], array[high] = array[high], array[start]
   return high
 
-'''
-def quicksort_iterative(array):
-  stack = [(0, len(array) - 1)]
-  while stack:
-    low, high = stack.pop()
-    if low < high:
-      pivot = partition(array, low, high)
-      stack.append((low, pivot - 1))
-      stack.append((pivot + 1, high))
-
-def partition(array, low, high):
-  pivot = array[random.randint(low, high)]
-  i = low - 1
-  for j in range(low, high):
-    if array[j] <= pivot:
-      i += 1
-    array[i], array[j] = array[j], array[i]
-  array[i + 1], array[high] = array[high], array[i + 1]
-  return i + 1
-'''
-
-def quicksort_iterative1(array):
-  stack1 = [(0, len(array) - 1)]
-  while stack1:
-    low, high = stack1.pop()
-    if low < high:
-      pivot = partition1(array, low, high)
-      stack1.append((low, pivot - 1))
-      stack1.append((pivot + 1, high))
-
-def partition1(array, low, high):
-  pivot = array[low]
-  i = low - 1
-  for j in range(low, high):
-    if array[j] <= pivot:
-      i += 1
-    array[i], array[j] = array[j], array[i]
-  array[i + 1], array[high] = array[high], array[i + 1]
-  return i + 1
-
 
 with open("ex2.json","r") as infile:
   json_obj = json.load(infile)
@@ -77,12 +37,6 @@ for i in range(len(json_obj)):
   func1(json_obj[i], 0, len(json_obj[i])-1)
   stop = time.time()
   print(f"recursive: {stop-start}\n")
-
-  start = time.time()
-  quicksort_iterative1(json_obj[i])
-  stop = time.time()
-  print(f"iterative: {stop-start}\n")
-
   results1.append((len(json_obj[i]), stop-start))
 
 print(results1)
